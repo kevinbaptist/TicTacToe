49d3fff6361fa615517bb0d7fe4a0a4ec2c81b37
Kevin <kbaptista258@gmail.com>
2020-08-25 17:53:25 +0100

    Initial Commit

---
+++ android/src/pt/ladon/games/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package pt.ladon.games;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import pt.ladon.games.TicTacToe;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TicTacToe(), config);
+	}
+}
